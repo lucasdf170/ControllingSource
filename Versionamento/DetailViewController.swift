70d3db9147d68ad205496fd5efd20ee9783da994
Lucas Batista <lucasdf170@hotmail.com>
2015-08-31 15:43:21 -0300

    segundo comió feliz

--- Versionamento/DetailViewController.swift
+++ Versionamento/DetailViewController.swift
@@ -33,11 +33,13 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //Ola Mundo !
     }
 
 
